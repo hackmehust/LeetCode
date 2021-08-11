615738825a84c7e5d14cbf2af4a82e446530ac00
Ngo Huy Thang 20175791 <thang.nh175791@sis.hust.edu.vn>
2021-08-11 18:43:26 +0700

    8/11/2021

---
+++ HowToDebug.java
@@ -0,0 +1,20 @@
+public class HowToDebug {
+
+    public static void F(int a) {
+        a++;
+        System.out.println("Check a: " + a);
+    }
+
+    public static void main(String[] args) {
+        int a = 0;
+        a++;
+        int b = 1;
+        int[] arr = {1,2,33};
+        for (int i = 0; i < 10; i++) {
+            a++;
+            System.out.println("a = " + a);
+            F(a);
+        }
+        System.out.println("Done!");
+    }
+}
